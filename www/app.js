51442974e5b275a1c4dc7b6ef6e505f5341649a0
Dianyi Yang <kv9898@rubuky.com>
2024-10-08 15:39:05 -0400

    fix theme changing

--- www/app.js
+++ www/app.js
@@ -2,21 +2,13 @@ $(document).on("keypress", function(event) {
     Shiny.setInputValue("keyid", event.keyCode);
 });
 
-//window.parent.addEventListener("quarto-color-mode", function(event) {
-  // Shiny.setInputValue('quarto_mode', event.detail.mode);
-//  document.documentElement.dataset.bsTheme = event.detail.mode;
-//  if (event.detail.mode === "dark") {
-//      document.documentElement.style.setProperty('--bs-body-bg', "#16242f");
-//  }  else if (event.detail.mode === "light") {
-//      document.documentElement.style.setProperty('--bs-body-bg', "#f9fffe");
-//  }
-//})
-
 window.addEventListener('message', function(e) {
     // Check the origin of the sender
     if (e.data === 'light-mode') {
+        document.documentElement.dataset.bsTheme = "light";
         document.documentElement.style.setProperty('--bs-body-bg', "#f9fffe");
     } else if (e.data === 'dark-mode') {
+        document.documentElement.dataset.bsTheme = "dark";
         document.documentElement.style.setProperty('--bs-body-bg', "#16242f");
     }
   }, false);
